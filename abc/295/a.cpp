6071b6f9be60658973e65b68e9c7fd9d86142f04
v8yte <buihachi.nishi@gmail.com>
2023-05-08 23:55:45 +0900

    A

--- abc/295/a.cpp
+++ abc/295/a.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
-
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++){
+         string W;
+         cin >> W;
+         if (W == "and" || W == "not" || W == "that" || W == "the" || W == "you"){
+             cout << "Yes" << endl;
+             return 0;
+         }
+    }
+    cout << "No" << endl;
     return 0;
 }
