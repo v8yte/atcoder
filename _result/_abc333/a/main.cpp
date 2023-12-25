9b63efcc643a796613fd6f943363f06f1b9493da
v8yte <buihachi.nishi@gmail.com>
2023-12-25 10:19:46 +0900

    abc333 a

---
+++ _result/_abc333/a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll n;
+    cin >> n;
+    for (ll i = 0; i < n - 1; i++){
+        cout << n;
+    }
+    cout << n << endl;
+    return 0;
+}
