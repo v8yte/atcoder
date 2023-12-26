abab72b6484bca0c812d8343e44e9422605fba63
v8yte <buihachi.nishi@gmail.com>
2023-12-26 23:49:14 +0900

    abc332 b

--- _result/_abc332/b/main.cpp
+++ _result/_abc332/b/main.cpp
@@ -3,28 +3,24 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    int k,g,m,G,M;
-    cin >> k >> g >> m;
-    G = 0;
-    M = 0;
-    for (int i = 0; i < k; i++){
-        if (G == g){
-            G = 0;
+    int K,G,M;
+    cin >> K >> G >> M;
+    int x = 0;
+    int y = 0;
+    int z;
+    for (int i = 0; i < K; i++) {
+        if(x == G) {
+            x = 0;
             continue;
         }
-        if (M == 0) {
-            M = m;
+        if(y == 0) {
+            y = M;
             continue;
         }
-
-        if (g - G > M){
-            G += M;
-            M = 0;
-        }else{
-            M -= g - G;
-            G = g;
-        }
+        z = min(y,G - x);
+        x += z;
+        y -= z;
     }
-    cout << G << " " << M << endl;
+    cout << x << " " << y << endl;
     return 0;
 }
