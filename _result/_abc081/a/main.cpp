a98229a10a7001eb35a382cd5ee0dab817f56436
v8yte <buihachi.nishi@gmail.com>
2023-12-30 10:35:49 +0900

    abc081 a

--- _result/_abc081/a/main.cpp
+++ _result/_abc081/a/main.cpp
@@ -6,8 +6,8 @@ int main() {
     string s;
     cin >> s;
     int count = 0;
-    for (int i = 0; i < 3; i++) {
-        if (int(s[i]) - 48 == 1) count++;
+    for(auto i : s){
+        if(i == '1') count++;
     }
     cout << count << endl;
     return 0;
