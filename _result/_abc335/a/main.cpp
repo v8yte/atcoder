fc18d460753faf44a3e4686b0840e406195d1a01
v8yte <buihachi.nishi@gmail.com>
2024-01-06 21:05:51 +0900

    abc335 a

---
+++ _result/_abc335/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s.replace(s.size()-1,1,"4") << endl;
+    return 0;
+}
