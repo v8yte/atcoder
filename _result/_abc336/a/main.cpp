c40013a3deaaaf97ab39d4893d0b87f125553c7d
v8yte <buihachi.nishi@gmail.com>
2024-01-14 21:03:01 +0900

    abc336 a

---
+++ _result/_abc336/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << "L";
+    for (int i = 0; i < N; i++) {
+        cout << "o";
+    }
+    cout << "ng" << endl;
+    return 0;
+}
